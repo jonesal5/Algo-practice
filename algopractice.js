7e68bd176e0885e65ad71930fd749c9f6045c74a
Alyssa <77090918+jonesal5@users.noreply.github.com>
2021-06-07 12:52:33 -0400

    Add files via upload

---
+++ algopractice.js
@@ -0,0 +1,15 @@
+// Algo Loop 1:
+for(vari = 1; i < 20; i+=2){
+        console.log(i);
+    }
+    1,3,5,7,9,11,13,15,17,19
+
+// Algo Loop 2:
+    (var num;)
+    (var sum=0;)
+
+for(num= 1; num <= 5; num++)
+        {sum = sum + num;
+        console.log("num": num "," "sum": sum);
+        console.log= 1
+}
